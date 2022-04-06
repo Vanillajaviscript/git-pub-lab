24ee6a08a3834ac11b9eabffea9e136f04603cc0
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-04-06 01:24:54 -0700

    adds link to files and drink list to html

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const drinks = require('./models/drinks.js')
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the Github App!');
+    res.send('Root directory');
+});
+
+app.get('/drinks', (req, res) => {
+    res.render('drinks_index.ejs', {allDrinks: drinks});
+})
+
+app.get('/drinks/:id', (req, res) => {
+    res.render('drinks_index.ejs', {allDrinks: drinks[req.params.id]})
+})
+
+app.use((req, res) => {
+    res.status(404).render('404');
 })
 
 app.listen(PORT, () => {
     console.log(`Server up on ${PORT}`);
 });
-
-app.get('/drinks', (req, res) => {
-    res.send(drinks)
-})
-
