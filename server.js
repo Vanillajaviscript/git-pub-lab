8ea75855f6be93fc0d8bf57afac40abac525586c
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-04-08 15:48:51 -0700

    adds change from jpg to png

--- server.js
+++ server.js
@@ -12,18 +12,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/drinks', (req, res) => {
-    // res.json(drinks)
     res.render('drinks_index.ejs', {
         allDrinks: drinks
     });
 })
 
-// app.get('/drinks', (req, res) => {
-//     // res.send(req.params.id)
-//     res.render('drinks_s.ejs', {allDrinks: drinks[req.params.id]})
-// })
-
 app.get('/drinks/:id', (req, res) => {
+    
     res.render('drinks_show.ejs', {drink: drinks[req.params.id]})
 })
 
